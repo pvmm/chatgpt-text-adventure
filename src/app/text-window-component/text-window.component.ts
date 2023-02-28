3fe144154e2182b7e4564717755dacee973c1f24
Pedro de Medeiros <pedro.medeiros@gmail.com>
2023-02-28 07:34:22 -0300

    create a terminal prompt and improve text position

--- src/app/text-window-component/text-window.component.ts
+++ src/app/text-window-component/text-window.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { ViewChild } from "@angular/core";
+import { ElementRef } from "@angular/core";
 
 @Component({
   selector: 'text-window',
@@ -6,5 +8,21 @@ import { Component } from '@angular/core';
   styleUrls: ['./text-window.component.less']
 })
 export class TextWindowComponent {
+  @ViewChild("userInput", {static: false})
+  userInput!: ElementRef;
 
+  @ViewChild("consoleOutput", {static: false})
+  consoleOutput!: ElementRef;
+
+  on_enter_key_up(event: any) {
+    // TODO: send command to chatGPT
+    let consoleOutput = this.consoleOutput.nativeElement;
+    let userInput = this.userInput.nativeElement;
+    consoleOutput.innerText += "\n∷ " + userInput.value;
+    userInput.value = "";
+  }
+ 
+  on_input_change() {
+    
+  }
 }
